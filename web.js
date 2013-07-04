737b276de187ebcb1178e79958a44bd8c1a21c4a
Piotr Korgol <piotr.korgol@gmail.com>
2013-07-04 14:57:53 +0200

    Web js modification

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     fs.readFile('/index.html', function(err, data){
 	response.send(data.toString());
-    }
+    });
 });
 
 var port = process.env.PORT || 5000;
